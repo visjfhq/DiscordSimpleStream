bb309ff0dc96e3fc4fdd68d5d1dd25cee43998ea
Us3rWeb <76919285+Us3rWeb@users.noreply.github.com>
2021-08-25 11:09:16 +0200

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const client = new Discord.Client()
 
 
 client.on("ready", async() => {
-    console.log(`[INFO]`.green + ` Le stream`.white + ` a commencé.`.green + `\n Discord Simple Stream Tool by`.white +  ` "0x38" Noro`.red + ` & `.white + `L0wki`.red)
+    console.log(`[INFO]`.green + ` Le stream`.white + ` a commencé.`.green + `\n Discord Simple Stream Tool by`.white +  ` "0x38" Noro`.red + ` & `.white + `Us3r`.red)
     client.user.setActivity(status, {type: 'STREAMING', url: url})
     client.user.setStatus('online')
 })
