1c6223b93ca27948ea0dc074ef6bf9fe272b7341
Visual1337 <76919285+Visual1337@users.noreply.github.com>
2021-09-26 08:28:53 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-cls && color 2 && title DiscordSimpleStream by Noro & Us3r && echo off && cls
+cls && color 2 && title DiscordSimpleStream by Noro & Visual && echo off && cls
 node main.js
 pause>nul
